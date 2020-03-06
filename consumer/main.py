45a113e652ca1cc9fe27232264820ba323d50908
ban367 <ban367@gosoly.com>
2020-03-06 15:08:45 +0900

    :+1: change to json format transmission

--- consumer/main.py
+++ consumer/main.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 from kafka.structs import OffsetAndMetadata, TopicPartition
+import json
 import time
 
 
@@ -9,15 +10,16 @@ def main():
             consumer = KafkaConsumer(
                 'test',
                 bootstrap_servers=['kafka:9092'],
-                auto_offset_reset="earliest",
-                group_id='1')
+                auto_offset_reset='earliest',
+                group_id='1',
+                value_deserializer=lambda x: json.loads(x.decode('utf-8')))
             break
         except Exception:
-            print("connection error")
+            print('connection error')
             time.sleep(3)
 
     for message in consumer:
-        print(message.value.decode())
+        print(message.value)
 
         tp = TopicPartition(message.topic, message.partition)
         offsets = {tp: OffsetAndMetadata(message.offset, None)}
--- procuer/main.py
+++ procuer/main.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+import json
 import time
 
 
@@ -6,17 +7,18 @@ def main():
     while True:
         try:
             producer = KafkaProducer(
-                bootstrap_servers='kafka:9092')
+                bootstrap_servers='kafka:9092',
+                value_serializer=lambda x: json.dumps(x).encode('utf-8'))
             break
         except Exception:
-            print("connection error")
+            print('connection error')
             time.sleep(3)
     send_count = 1
 
     while True:
         result = producer.send(
-            'test', ("send_count: " + str(send_count)).encode()
-            ).get(timeout=60)
+            'test', {'send_count': send_count, 'message': 'test'}
+        ).get(timeout=60)
         print(result)
         time.sleep(1)
         send_count += 1
